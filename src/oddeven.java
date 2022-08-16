8aeb99a77b0bd1150f791445b4c9cbd23977cfcf
siyanakoriya <siyanakoriya122@gmail.com>
2022-08-16 20:22:31 +0530

    oddevene

---
+++ src/oddeven.java
@@ -0,0 +1,13 @@
+public class oddeven
+{
+    public static void main(String[] args) {
+        int number = 13;
+
+
+
+        if (number % 2 == 0)
+            System.out.println(number + " is Even");
+        else
+            System.out.println(number + " is odd");
+    }
+}
