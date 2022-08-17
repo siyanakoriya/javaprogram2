a3308be1bbc2c2e8705b5c9dcec958db6c67c2b5
siyanakoriya <siyanakoriya122@gmail.com>
2022-08-17 17:38:03 +0530

    prime

---
+++ src/prime.java
@@ -0,0 +1,16 @@
+public class prime {
+    public static void main(String[] args) {
+        int n = 11, count = 0;
+        for (int i = 1; i <= n; i++) {
+            if ( n % i  == 0) {
+
+                count++;
+            }
+        }
+        if (count == 2)
+            System.out.println("the number is prime");
+        else
+            System.out.println("not prime");
+
+
+    }   }
